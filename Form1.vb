c643b686ee620aac726550c1ee6092b1f7a7a29c
Anu6is <curtis.mayers@gmail.com>
2023-12-03 15:57:47 -0400

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports LiveWallpaperEngine
+Imports LiveWallpaperEngineRender.Renders
+
+Public Class Form1
+    Private VideoRender As VideoRender
+
+    Private Sub BtnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        Dim screen = Windows.Forms.Screen.PrimaryScreen
+
+        VideoRender = New VideoRender()
+
+        VideoRender.Init(screen)
+        VideoRender.Play("./Video.mp4")
+
+        Dim success = LiveWallpaperEngineManager.Show(VideoRender, screen)
+    End Sub
+
+    Private Sub BtnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        LiveWallpaperEngineManager.Close(VideoRender)
+    End Sub
+End Class
+
